395094e4a86b0afbb997a6505851b1d8c17bb41d
William <william.flynt@gmail.com>
2019-10-07 14:59:33 -0700

    Update DataFrame operations to return values

--- classifier/pd_operations.py
+++ classifier/pd_operations.py
@@ -1,5 +1,4 @@
 """Small functions to clean up readability of the dataframe processing"""
-import numpy as np
 
 
 def count_digits(s):
@@ -22,7 +21,21 @@ def count_other_text(s):
     return len(s) - count_digits(s) - count_letters(s) - count_spaces(s)
 
 
-def frac_fxn(fxn, rows):
-    """calculates the numerator and denominator for functions requiring fractions"""
-    s = 0  # prevents an error
-    return np.sum([fxn(str(s)) for s in rows]) / np.sum([len(str(s)) for s in rows])
+def frac_fxn(fxn, rows) -> float:
+    """
+    calculates the numerator and denominator for functions requiring fractions
+
+    1. Join the rows as one string (with no whitespace to avoid adding characters)
+    2. Calculate the fraction function for the joined string
+    3. Divide by the number of characters in the joined string
+    4. Return that result
+
+    Note: An empty column will produce a ZeroDivisionError - so let's just return -1
+    """
+    aggregated = "".join([str(r) for r in rows])
+    fxn_result = fxn(aggregated)
+    try:
+        result = fxn_result/len(aggregated)
+    except ZeroDivisionError:
+        return -1
+    return result
--- classifier/utils.py
+++ classifier/utils.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 import numpy as np
 import pandas as pd
@@ -18,21 +18,26 @@ from classifier.pd_operations import (
 def load_csv(document: File) -> pd.DataFrame:
     """load a CSV from a Source.document field to a dataframe"""
     with document.open("r") as f:
-        df = pd.read_csv(f)
+        df = pd.read_csv(f, keep_default_na=False)
     return df
+
+
+def label_or_reject(label_set: Tuple[str, str]) -> str:
+    """return the joined label or 'reject' if the label is blank"""
+    return "".join(label_set) or "reject"
 
 
 def load_labels(source: Source) -> Dict[str, List[str]]:
     """make the list of labels, where Column index maps to the position in each list"""
     labels = [c.labels for c in source.column_set.all()]
     main, sub = zip(*labels)
-    labels = {"main": main, "sub": sub, "label": ["".join(item) for item in labels]}
+    labels = {"main": main, "sub": sub, "label": [label_or_reject(x) for x in labels]}
     return labels
 
 
 def analyze_dataframe(df: pd.DataFrame, labels: Dict[str, List[str]]) -> pd.DataFrame:
     """analyze all columns in a DataFrame and return a DataFrame of results"""
-
+    # Make a container for the results of our analysis
     _df = pd.DataFrame()
 
     # write labels for magical mAcHiNe LeArNiNg
@@ -41,26 +46,26 @@ def analyze_dataframe(df: pd.DataFrame, labels: Dict[str, List[str]]) -> pd.Data
     _df['label'] = labels["label"]
 
     # mean token metrics aggregated across each column
-    _df['mean_token_count'] = df.apply(lambda rows: np.mean([len(str(s).split()) for s in rows]))
-    _df['mean_token_length'] = df.apply(lambda rows: np.mean([len(i.split()) for s in rows for i in str(s)]))
+    _df['mean_token_count'] = df.apply(lambda rows: np.mean([len(str(s).split()) for s in rows])).values
+    _df['mean_token_length'] = df.apply(lambda rows: np.mean([len(i.split()) for s in rows for i in str(s)])).values
 
     # fraction metrics aggregated across each column
-    _df['frac_digit'] = df.apply(lambda rows: frac_fxn(count_digits, rows))
-    _df['frac_alpha'] = df.apply(lambda rows: frac_fxn(count_letters, rows))
-    _df['frac_space'] = df.apply(lambda rows: frac_fxn(count_spaces, rows))
-    _df['frac_other_text'] = df.apply(lambda rows: frac_fxn(count_other_text, rows))
+    _df['frac_digit'] = df.apply(lambda rows: frac_fxn(count_digits, rows)).values
+    _df['frac_alpha'] = df.apply(lambda rows: frac_fxn(count_letters, rows)).values
+    _df['frac_space'] = df.apply(lambda rows: frac_fxn(count_spaces, rows)).values
+    _df['frac_other_text'] = df.apply(lambda rows: frac_fxn(count_other_text, rows)).values
 
     # line length metrics
-    _df['mean_line_length'] = df.apply(lambda rows: np.mean([len(str(s)) for s in rows]))
-    _df['median_line_length'] = df.apply(lambda rows: np.median([len(str(s)) for s in rows]))
-    _df['std_line_length'] = df.apply(lambda rows: np.std([len(str(s)) for s in rows]))
+    _df['mean_line_length'] = df.apply(lambda rows: np.mean([len(str(s)) for s in rows])).values
+    _df['median_line_length'] = df.apply(lambda rows: np.median([len(str(s)) for s in rows])).values
+    _df['std_line_length'] = df.apply(lambda rows: np.std([len(str(s)) for s in rows])).values
 
     # unique char / token metrics
-    _df['unq_char_count'] = df.apply(lambda rows: len(set([i for s in rows for i in str(s)])))
-    _df['unq_token_count'] = df.apply(lambda rows: len(set([str(s) for s in rows])))
+    _df['unq_char_count'] = df.apply(lambda rows: len(set([i for s in rows for i in str(s)]))).values
+    _df['unq_token_count'] = df.apply(lambda rows: len(set([str(s) for s in rows]))).values
 
     # number of rows tagged on each column
-    _df['row_count'] = df.apply(lambda rows: len(rows))
+    _df['row_count'] = df.apply(lambda rows: len(rows)).values
 
     return _df.transpose()
 
